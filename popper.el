44752c5121801502405300ca86cbdbfdfd86fe9c
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2021-03-08 01:25:31 -0800

    Better test for removing from display-buffer-alist

--- popper.el
+++ popper.el
@@ -459,9 +459,9 @@ details on how to designate buffer types as popups."
           popper-open-popup-alist nil)
     (remove-hook 'window-configuration-change-hook #'popper-update-popups)
     (setq display-buffer-alist
-          (cl-remove '(popper-display-control-p)
+          (cl-remove 'popper-display-control-p
                      display-buffer-alist
-                     :test (lambda (a b) (eq (car a) (car b)))))))
+                     :key 'car))))
 
 (provide 'popper)
 ;;; popper.el ends here
