2f0ae1f897557aa060c996d21b23caf6bf65cf0a
Vincent Zhang <seagle0128@gmail.com>
2022-03-07 14:30:21 +0800

    Check whether mode-line-format is a list.

--- popper.el
+++ popper.el
@@ -467,10 +467,11 @@ a popup buffer to open."
   (when (and popper-mode-line mode-line-format)
     (if (member popper-mode-line mode-line-format)
         mode-line-format
-      (append (cl-subseq (default-value 'mode-line-format) 0 popper-mode-line-position)
-              (list popper-mode-line
-                    (nthcdr popper-mode-line-position
-                            (default-value 'mode-line-format)))))))
+      (and (listp mode-line-format)
+           (append (cl-subseq (default-value 'mode-line-format) 0 popper-mode-line-position)
+                   (list popper-mode-line
+                         (nthcdr popper-mode-line-position
+                                 (default-value 'mode-line-format))))))))
 
 (defun popper--restore-mode-lines (win-buf-alist)
   "Restore the default value of `mode-line-format'.
